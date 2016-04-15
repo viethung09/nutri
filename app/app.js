f93271c4f6bf6d3504f6c11f1f213f06da7facf4
Hung Do Viet <viethung097@gmail.com>
2016-04-15 15:59:46 +0700

    Add bootstrap and resource mock

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
 (function () {
   "use strict";
-  var app = angular.module("productManagement",[]);
+  var app = angular.module("productManagement",["common.services", "productResourceMock"]);
 }());
---
+++ common/services/productResource.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+
+  angular.module("common.services")
+         .factory( "productResource", ["$resource", productResource] );
+
+  function productResource($resource) {
+    return $resource("/api/products/:productId")
+  }
+
+}());
